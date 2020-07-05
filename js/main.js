cfe926b995091c70e3fd871dc2f366039b623536
Foodyloan <elvinakpan1@gmail.com>
2020-07-05 10:35:01 +0100

    foodsite
    foodsite

---
+++ js/main.js
@@ -0,0 +1,558 @@
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide'
+ });
+
+(function($) {
+
+	"use strict";
+
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+
+	$(window).stellar({
+    responsive: true,
+    parallaxBackgrounds: true,
+    parallaxElements: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,
+    scrollProperty: 'scroll'
+  });
+
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	// loader
+	var loader = function() {
+		setTimeout(function() { 
+			if($('#ftco-loader').length > 0) {
+				$('#ftco-loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader();
+
+	// Scrollax
+   $.Scrollax();
+
+	var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+	
+		$('.carousel-testimony').owlCarousel({
+			center: true,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 3
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
+
+	};
+	carousel();
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		// 	 timer;
+		// clearTimeout(timer);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			// timer;
+		// timer = setTimeout(function(){
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+			$this.find('.dropdown-menu').removeClass('show');
+		// }, 100);
+	});
+
+
+	$('#dropdown04').on('show.bs.dropdown', function () {
+	  console.log('show');
+	});
+
+	// scroll
+	var scrollWindow = function() {
+		$(window).scroll(function(){
+			var $w = $(this),
+					st = $w.scrollTop(),
+					navbar = $('.ftco_navbar'),
+					sd = $('.js-scroll-wrap');
+
+			if (st > 150) {
+				if ( !navbar.hasClass('scrolled') ) {
+					navbar.addClass('scrolled');	
+				}
+			} 
+			if (st < 150) {
+				if ( navbar.hasClass('scrolled') ) {
+					navbar.removeClass('scrolled sleep');
+				}
+			} 
+			if ( st > 350 ) {
+				if ( !navbar.hasClass('awake') ) {
+					navbar.addClass('awake');	
+				}
+				
+				if(sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if ( st < 350 ) {
+				if ( navbar.hasClass('awake') ) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if(sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
+
+	
+	var counter = function() {
+		
+		$('#section-counter').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+						console.log(num);
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
+					);
+				});
+				
+			}
+
+		} , { offset: '95%' } );
+
+	}
+	counter();
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.ftco-animate').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .ftco-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							var effect = el.data('animate-effect');
+							if ( effect === 'fadeIn') {
+								el.addClass('fadeIn ftco-animated');
+							} else if ( effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft ftco-animated');
+							} else if ( effect === 'fadeInRight') {
+								el.addClass('fadeInRight ftco-animated');
+							} else {
+								el.addClass('fadeInUp ftco-animated');
+							}
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
+
+	// navigation
+	var OnePageNav = function() {
+		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
+		 	e.preventDefault();
+
+		 	var hash = this.hash,
+		 			navToggler = $('.navbar-toggler');
+		 	$('html, body').animate({
+		    scrollTop: $(hash).offset().top
+		  }, 700, 'easeInOutExpo', function(){
+		    window.location.hash = hash;
+		  });
+
+
+		  if ( navToggler.is(':visible') ) {
+		  	navToggler.click();
+		  }
+		});
+		$('body').on('activate.bs.scrollspy', function () {
+		  console.log('nice');
+		})
+	};
+	OnePageNav();
+
+
+	// magnific popup
+	$('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+     gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      verticalFit: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+
+    fixedContentPos: false
+  });
+
+
+
+	var goHere = function() {
+
+		$('.mouse-icon').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html,body').animate({
+				scrollTop: $('.goto-here').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+	};
+	goHere();
+
+
+	function makeTimer() {
+
+		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		endTime = (Date.parse(endTime) / 1000);
+
+		var now = new Date();
+		now = (Date.parse(now) / 1000);
+
+		var timeLeft = endTime - now;
+
+		var days = Math.floor(timeLeft / 86400); 
+		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+		if (hours < "10") { hours = "0" + hours; }
+		if (minutes < "10") { minutes = "0" + minutes; }
+		if (seconds < "10") { seconds = "0" + seconds; }
+
+		$("#days").html(days + "<span>Days</span>");
+		$("#hours").html(hours + "<span>Hours</span>");
+		$("#minutes").html(minutes + "<span>Minutes</span>");
+		$("#seconds").html(seconds + "<span>Seconds</span>");		
+
+}
+
+setInterval(function() { makeTimer(); }, 1000);
+
+
+
+})(jQuery);
+
+
+$(".fa-search").click(function(){
+   $(".icon").toggleClass("active");
+  $("input[type='text']").toggleClass("active");
+});
+
+
+
+
+
+
+jQuery(document).ready(function($){
+  var $form_modal = $('.user-modal'),
+    $form_login = $form_modal.find('#login'),
+    $form_signup = $form_modal.find('#signup'),
+    $form_forgot_password = $form_modal.find('#reset-password'),
+    $form_modal_tab = $('.switcher'),
+    $tab_login = $form_modal_tab.children('li').eq(0).children('a'),
+    $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
+    $forgot_password_link = $form_login.find('.form-bottom-message a'),
+    $back_to_login_link = $form_forgot_password.find('.form-bottom-message a'),
+    $main_nav = $('.main-nav');
+
+  //open modal
+  $main_nav.on('click', function(event){
+
+    if( $(event.target).is($main_nav) ) {
+      // on mobile open the submenu
+      $(this).children('ul').toggleClass('is-visible');
+    } else {
+      // on mobile close submenu
+      $main_nav.children('ul').removeClass('is-visible');
+      //show modal layer
+      $form_modal.addClass('is-visible'); 
+      //show the selected form
+      ( $(event.target).is('.signup') ) ? signup_selected() : login_selected();
+    }
+
+  });
+
+  //close modal
+  $('.user-modal').on('click', function(event){
+    if( $(event.target).is($form_modal) || $(event.target).is('.close-form') ) {
+      $form_modal.removeClass('is-visible');
+    } 
+  });
+  //close modal when clicking the esc keyboard button
+  $(document).keyup(function(event){
+      if(event.which=='27'){
+        $form_modal.removeClass('is-visible');
+      }
+    });
+
+  //switch from a tab to another
+  $form_modal_tab.on('click', function(event) {
+    event.preventDefault();
+    ( $(event.target).is( $tab_login ) ) ? login_selected() : signup_selected();
+  });
+
+  //hide or show password
+  $('.hide-password').on('click', function(){
+    var $this= $(this),
+      $password_field = $this.prev('input');
+    
+    ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') : $password_field.attr('type', 'password');
+    ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
+    //focus and move cursor to the end of input field
+    $password_field.putCursorAtEnd();
+  });
+
+  //show forgot-password form 
+  $forgot_password_link.on('click', function(event){
+    event.preventDefault();
+    forgot_password_selected();
+  });
+
+  //back to login from the forgot-password form
+  $back_to_login_link.on('click', function(event){
+    event.preventDefault();
+    login_selected();
+  });
+
+  function login_selected(){
+    $form_login.addClass('is-selected');
+    $form_signup.removeClass('is-selected');
+    $form_forgot_password.removeClass('is-selected');
+    $tab_login.addClass('selected');
+    $tab_signup.removeClass('selected');
+  }
+
+  function signup_selected(){
+    $form_login.removeClass('is-selected');
+    $form_signup.addClass('is-selected');
+    $form_forgot_password.removeClass('is-selected');
+    $tab_login.removeClass('selected');
+    $tab_signup.addClass('selected');
+  }
+
+  function forgot_password_selected(){
+    $form_login.removeClass('is-selected');
+    $form_signup.removeClass('is-selected');
+    $form_forgot_password.addClass('is-selected');
+  }
+
+  //REMOVE THIS - it's just to show error messages 
+  $form_login.find('input[type="submit"]').on('click', function(event){
+    event.preventDefault();
+    $form_login.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+  });
+  $form_signup.find('input[type="submit"]').on('click', function(event){
+    event.preventDefault();
+    $form_signup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+  });
+
+
+  //IE9 placeholder fallback
+  //credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+  if(!Modernizr.input.placeholder){
+    $('[placeholder]').focus(function() {
+      var input = $(this);
+      if (input.val() == input.attr('placeholder')) {
+        input.val('');
+        }
+    }).blur(function() {
+      var input = $(this);
+        if (input.val() == '' || input.val() == input.attr('placeholder')) {
+        input.val(input.attr('placeholder'));
+        }
+    }).blur();
+    $('[placeholder]').parents('form').submit(function() {
+        $(this).find('[placeholder]').each(function() {
+        var input = $(this);
+        if (input.val() == input.attr('placeholder')) {
+          input.val('');
+        }
+        })
+    });
+  }
+
+});
+
+
+//credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+jQuery.fn.putCursorAtEnd = function() {
+  return this.each(function() {
+      // If this function exists...
+      if (this.setSelectionRange) {
+          // ... then use it (Doesn't work in IE)
+          // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+          var len = $(this).val().length * 2;
+          this.setSelectionRange(len, len);
+      } else {
+        // ... otherwise replace the contents with itself
+        // (Doesn't work in Google Chrome)
+          $(this).val($(this).val());
+      }
+  });
+};
+
+	$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function () {
+   //1 second of animation time
+   //html works for FFX but not Chrome
+   //body works for Chrome but not FFX
+   //This strange selector seems to work universally
+   $("html, body").animate({scrollTop: 0}, 1);
+});
+
+	
+		
+			// /!\ Optional JS for Autoplay /!\
+
+// AUTOPLAY SLIDER CSS
+  var nbSlide = $(".slides").length;
+  var i = 1;
+  function sliderAutoplay(){
+    if(i == nbSlide+1) {
+      i = 1;
+      $("#button-"+i).prop("checked", true).trigger("click");
+    } else {
+      $("#button-"+i).prop("checked", true).trigger("click");
+    }
+    i++;
+    // durée d'une slide
+    timeout = setTimeout(sliderAutoplay,8000); 
+  }
+  sliderAutoplay();
+
+// AUTOPLAY STOP QUAND ON CHANGE MANUELLEMENT DE SLIDE
+  $('#slideshow-wrap input').click(function(){
+    var id = $(this).attr("id");
+    clearTimeout(timeout);
+    //console.log(id.substr(-1));
+  });
+			
+	  
+	
+	var flkty = new Flickity( '.main-gallery', {
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  prevNextButtons: false,
+  autoPlay: 9000
+});
+	
