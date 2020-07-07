2d9ce62259bc6f360887da0895f6cf490b0af2e0
foodiesonly <67858399+foodiesonly@users.noreply.github.com>
2020-07-07 16:42:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -86,29 +86,7 @@
 	    }
 		});
 	
-		$('.carousel-testimony').owlCarousel({
-			center: true,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 3
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
-
-	};
-	carousel();
+		
 
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
